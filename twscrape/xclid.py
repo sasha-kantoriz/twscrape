74b992fc6a66e3e01d681c4490b9210861c4c8f5
Oleksandr Kantoriz <no.money.no.honey5151@gmail.com>
2026-04-10 09:16:40 +0300

    apply monkey patch

--- twscrape/xclid.py
+++ twscrape/xclid.py
@@ -213,17 +213,64 @@ async def parse_anim_idx(text: str) -> list[int]:
     return items
 
 
-def parse_anim_arr(soup: bs4.BeautifulSoup, vk_bytes: list[int]) -> list[list[float]]:
-    # https://github.com/fa0311/twitter-tid-deobf/blob/c4fd61c36/output/a.js#L18
-    els = list(soup.select("svg[id^='loading-x-anim'] g:first-child path:nth-child(2)"))
-    els = [str(x.get("d") or "").strip() for x in els]
-    if not els:
-        raise Exception("Couldn't get XClientTxId animation array")
+# def parse_anim_arr(soup: bs4.BeautifulSoup, vk_bytes: list[int]) -> list[list[float]]:
+#     # https://github.com/fa0311/twitter-tid-deobf/blob/c4fd61c36/output/a.js#L18
+#     els = list(soup.select("svg[id^='loading-x-anim'] g:first-child path:nth-child(2)"))
+#     els = [str(x.get("d") or "").strip() for x in els]
+#     if not els:
+#         raise Exception("Couldn't get XClientTxId animation array")
+#
+#     idx = vk_bytes[5] % len(els)
+#     dat = els[idx][9:].split("C")
+#     arr = [list(map(float, re.sub(r"[^\d]+", " ", x).split())) for x in dat]
+#     return arr
 
-    idx = vk_bytes[5] % len(els)
-    dat = els[idx][9:].split("C")
-    arr = [list(map(float, re.sub(r"[^\d]+", " ", x).split())) for x in dat]
-    return arr
+def _rextr(s: str, begin: str, end: str, pos: int) -> str | None:
+    end_idx = s.rfind(end, 0, pos)
+    if end_idx < 0:
+        return None
+    begin_idx = s.rfind(begin, 0, end_idx)
+    if begin_idx < 0:
+        return None
+    return s[begin_idx + len(begin):end_idx]
+
+
+def _fextr(s: str, begin: str, end: str, pos: int = 0) -> str | None:
+    start = s.find(begin, pos)
+    if start < 0:
+        return None
+    start += len(begin)
+    stop = s.find(end, start)
+    if stop < 0:
+        return None
+    return s[start:stop]
+
+
+async def parse_anim_idx(text: str) -> list[int]:
+    # New format: "ondemand.s" is a value in a name map, hash lives in a second
+    # map under the same key further in the HTML.
+    ondemand_pos = text.find('"ondemand.s"')
+    if ondemand_pos >= 0:
+        ondemand_key = _rextr(text, ",", ':', ondemand_pos)
+        if ondemand_key:
+            ondemand_s = _fextr(text, ondemand_key + ':"', '"', ondemand_pos)
+            if ondemand_s:
+                url = script_url("ondemand.s", f"{ondemand_s}a")
+                js_text = await get_tw_page_text(url)
+                items = [int(x.group(2)) for x in INDICES_REGEX.finditer(js_text)]
+                if items:
+                    return items
+
+    # Fallback: old format where the chunk map contains ondemand.s as a key.
+    scripts = list(get_scripts_list(text))
+    scripts = [u for u in scripts if "/ondemand.s." in u]
+    if not scripts:
+        raise Exception("Couldn't get XClientTxId scripts")
+    js_text = await get_tw_page_text(scripts[0])
+    items = [int(x.group(2)) for x in INDICES_REGEX.finditer(js_text)]
+    if not items:
+        raise Exception("Couldn't get XClientTxId indices")
+    return items
 
 
 async def load_keys(soup: bs4.BeautifulSoup) -> tuple[list[int], str]:
